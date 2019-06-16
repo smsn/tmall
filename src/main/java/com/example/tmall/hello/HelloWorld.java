5f619e1a92ebf1ec1312fb8e0ca1b29841fdfa11
smsn <rootsmsn@gmail.com>
2019-06-16 19:43:44 +0800

    first commit

---
+++ src/main/java/com/example/tmall/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.tmall.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HelloWorld
+ */
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello.html";
+    }
+}
