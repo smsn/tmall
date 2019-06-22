1bc62d1735a60a648a8510d80bbccb274ac65ed7
smsn <rootsmsn@gmail.com>
2019-06-22 14:20:35 +0800

    添加退出功能

--- src/main/java/com/example/tmall/controller/ForePageController.java
+++ src/main/java/com/example/tmall/controller/ForePageController.java
@@ -1,5 +1,7 @@
 package com.example.tmall.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -32,5 +34,11 @@ public class ForePageController {
     @GetMapping("/login")
     public String login() {
         return "fore/login";
+    }
+
+    @GetMapping("/forelogout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:home";
     }
 }
